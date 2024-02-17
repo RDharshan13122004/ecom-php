cd6bcf506703b1479413fbb40f9a1c8885930eef
RDharshan13122004 <rdharshansrinivas@gmail.com>
2024-02-17 20:01:11 +0530

    deleted folder

--- ecom/admin_area/logoutpages.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-    session_start();
-    session_unset();
-    session_destroy();
-    echo"<script>window.open('admin_login.php','_self')</script>";
-
-?>
--- ecom/includes/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    $con=mysqli_connect('localhost','root','','mystore');
-    if(!$con){
-        die(mysqli_error($con));
-    }
-?>

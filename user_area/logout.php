93c5f3cb19682d263adf960e70e9a7d51e16922b
RDharshan13122004 <rdharshansrinivas@gmail.com>
2024-02-17 20:42:51 +0530

    delete file

--- user_area/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-    session_start();
-    session_unset();
-    session_destroy();
-    echo"<script>window.open('../index.php','_self')</script>";
-
-?>
-
--- user_area/sp.js
+++
@@ -1,7 +0,0 @@
-const password = document.querySelector("#textPassword");
-const Checkbox = document.querySelector("#show");
-
-Checkbox.addEventListener('click',function(){
-    const type = password.getAttribute("type")==="password" ? "text":"password";
-    password.setAttribute("type",type);
-});
